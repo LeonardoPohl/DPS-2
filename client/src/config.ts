324db3add3c0f1b517049fc08ce6b9c5e4647298
Andras Schmelczer <schmelczer.andras@simonyi.bme.hu>
2021-12-07 14:34:48 +0100

    Fix websocket uri

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,5 +2,7 @@ import { isDevelopment } from "./helper/is-development";
 
 export const config = {
   webSocketPort: 7700,
-  baseUri: `${window.location.protocol}//${window.location.hostname}:7701`,
+  baseUri: isDevelopment
+    ? `${window.location.protocol}//${window.location.hostname}:7701`
+    : window.location.origin,
 };
