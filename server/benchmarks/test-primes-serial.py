141901e07858383bfc227289ab1f289b05f25b22
LeonardoPohl <pohly3@gmail.com>
2021-12-15 21:28:00 +0100

    Preperation for experiments

--- server/benchmarks/test-primes-serial.py
+++ server/benchmarks/test-primes-serial.py
@@ -1,36 +1,23 @@
 import logging
-
 from tqdm import tqdm
-
-
-def nth_prime(x):
-    n = 5000000
-    prime = [True for i in range(n + 1)]
-
-    p = 2
-    while p * p <= n:
-        # If prime[p] is not changed,
-        # then it is a prime
-        if prime[p] == True:
-
-            # Update all multiples of p
-            for i in range(p * p, n + 1, p):
-                prime[i] = False
-
-        p += 1
-
-    primes = []
-    for p in range(2, n + 1):
-        if prime[p]:
-            primes.append(p)
-
-    return primes[x]
+import pandas as pd
+from time import time
+from constants import nth_prime, n_reps, columns
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
-    numbers = list(range(1000))
-    results = list(tqdm(map(nth_prime, numbers), total=len(numbers)))
+    df = pd.DataFrame(columns=columns)
 
-    print(results)
+    numbers = list(range(1000))
+    for i in range(n_reps):    
+        start = time()
+
+        results = list(tqdm(map(nth_prime, numbers), total=len(numbers)))
+
+        end = time()
+        df.loc[len(df.index)] = ["Primes", True, n_reps, 0, end - start, sys.argv[1]]
+
+    df.to_csv('results.csv', mode='a', index=False)
+    # print(results)
--- server/benchmarks/test-primes.py
+++ server/benchmarks/test-primes.py
@@ -5,38 +5,22 @@ sys.path.append("..")
 import logging
 
 from distributed_execution import DistributedExecution
-
-
-def nth_prime(x):
-    n = 5000000
-    prime = [True for i in range(n + 1)]
-
-    p = 2
-    while p * p <= n:
-        # If prime[p] is not changed,
-        # then it is a prime
-        if prime[p] == True:
-
-            # Update all multiples of p
-            for i in range(p * p, n + 1, p):
-                prime[i] = False
-
-        p += 1
-
-    primes = []
-    for p in range(2, n + 1):
-        if prime[p]:
-            primes.append(p)
-
-    return primes[x]
-
+from constants import n_reps, nth_prime, rmse
+import pandas as pd
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-
+    corr_res = list(map(nth_prime, numbers), total=len(numbers))
+    
     numbers = list(range(1000))
+    
+    for i in range(n_reps):
 
-    with DistributedExecution() as d:
-        results = d.map(nth_prime, numbers, chunk_size=1)
+        start = time()
 
-    print(results)
+        with DistributedExecution() as d:
+            results = d.map(nth_prime, numbers, chunk_size=1)
+
+        end = time()
+
+        mserr = rmse(corr_res, results)
