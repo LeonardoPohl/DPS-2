05eca9b33717805c6e514b6ebc8a8f043f83dc9a
Andras Schmelczer <schmelczer.andras@simonyi.bme.hu>
2021-12-17 20:42:57 +0100

    Add cpu-overhead var

--- server/benchmarks/test-vec.py
+++ server/benchmarks/test-vec.py
@@ -3,28 +3,33 @@ import sys
 sys.path.append("..")
 
 import logging
+logging.basicConfig(level=logging.DEBUG)
 
 import numpy as np
 
 from distributed_execution import DistributedExecution
 from time import time
 
-SIZE = 4096
-VECTOR_COUNT = 10000
+SIZE = 1024
+VECTOR_COUNT = 100000
 REPETITIONS = 10
+CPU_OVERHEAD = 1
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
     for _ in range(REPETITIONS):
         A = np.random.random((SIZE, SIZE))
 
         def vector_multiplication(v: np.ndarray) -> np.ndarray:
-            return np.dot(A, v)
+            a = np.dot(A, v)
+            for j in range(CPU_OVERHEAD):
+                a = np.dot(A, a)
+            return a
 
         vectors = [np.random.random(SIZE) for _ in range(VECTOR_COUNT)]
-        correct_results = list(map(vector_multiplication, vectors))
         start = time()
-        with DistributedExecution(packages=["numpy"]) as d:
+    
+        with DistributedExecution(packages=["numpy"], timeout_in_seconds=5) as d:
             results = d.map(vector_multiplication, vectors, chunk_size=2)
+
         end = time()
         print(end - start)
